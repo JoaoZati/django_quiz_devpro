b91c449c42b1428b567e3e1bd11eb227fc6b2d06
JoaoZati <joaozati@gmail.com>
2021-11-02 10:19:08 -0300

    Part 2: - add game.html and index.html on urls.py and views.py;

--- main/urls.py
+++ main/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import base.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+    path('questions/', views.questions),
 ]
+
