395cb0f365156295537e1a2c79d3464205c950f5
JoaoZati <joaozati@gmail.com>
2021-11-02 12:46:03 -0300

    Part 2: - add Question into game context;

--- base/views.py
+++ base/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
+from base.models import Question
+
 
 def home(request):
     return render(request, 'base/index.html')
 
 
 def questions(request, index):
+    question = Question.objects.filter(available=True).order_by('id')[index - 1]
     context = {
         'index_question': index,
+        'question': question,
     }
     return render(request, 'base/game.html', context)
 
