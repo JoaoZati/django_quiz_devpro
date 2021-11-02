c155727a4201373e5053581d9ac466af330469c6
JoaoZati <joaozati@gmail.com>
2021-11-02 11:45:53 -0300

    Part 2: - create model Question;

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    content = models.TextField()
+    available = models.BooleanField(default=False)
+    answers = models.JSONField()
+    right_answer = models.IntegerField(choices=[
+        (0, 'A'),
+        (1, 'B'),
+        (2, 'C'),
+        (3, 'D'),
+    ])
