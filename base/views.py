89bb7ef687fb5e665957b40543a9650ab70cfb53
JoaoZati <joaozati@gmail.com>
2021-11-02 16:06:06 -0300

    Part 2; - Make 80/20 here to resolve most of the applications problems; - views.py: add session in home;

--- base/views.py
+++ base/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render, redirect
 
 from base.forms import ParticipantForm
-from base.models import Question
+from base.models import Question, Participant
 from django.urls import reverse # noqa
 
 
 def home(request):
     if request.method == 'POST':
-        form = ParticipantForm(request.POST)
-        if form.is_valid():
-            participant = form.save()
-            return redirect('/question/1')
-        context = {'form': form}
-        return render(request, 'base/index.html', context)
+        email = request.POST['email']
+
+        try:
+            participant = Participant.objects.get(email=email)
+        except Participant.DoesNotExist:
+            form = ParticipantForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('/question/1')
+            context = {'form': form}
+            return render(request, 'base/index.html', context)
+        request.session['participant_id'] = participant.id
+        return redirect('/question/1')
+
     return render(request, 'base/index.html')
 
 
