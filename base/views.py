a3ce02ea61bdf989252bf037de2b007fd3293ca2
JoaoZati <joaozati@gmail.com>
2021-11-02 18:59:36 -0300

    Part 2; - views.py: Fixed some inssues;

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 from base.forms import ParticipantForm
 from base.models import Question, Participant, Answer
-from django.urls import reverse # noqa
+from django.urls import reverse  # noqa
 
 
 def home(request):
@@ -15,17 +15,23 @@ def home(request):
             form = ParticipantForm(request.POST)
             if form.is_valid():
                 form.save()
+                participant = Participant.objects.get(email=email)
+                request.session['participant_id'] = participant.id
                 return redirect('/question/1')
             context = {'form': form}
             return render(request, 'base/index.html', context)
         request.session['participant_id'] = participant.id
-        return redirect('/question/1')
+        return redirect('/ranking/')
 
     return render(request, 'base/index.html')
 
 
-MAX_SCORE = 1000
+MAX_SCORE = 3
+
+
 def questions(request, index):
+    global MAX_SCORE
+
     try:
         participant_id = request.session['participant_id']
     except KeyError:
@@ -35,7 +41,6 @@ def questions(request, index):
         question = Question.objects.filter(available=True).order_by('id')[index - 1]
     except IndexError:
         return redirect('/ranking/')
-
     context = {
         'index_question': index,
         'question': question,
@@ -44,10 +49,14 @@ def questions(request, index):
     if request.method == 'POST':
         index_answer = int(request.POST['answer_index'])
         if index_answer == question.right_answer:
-            # add answer to db
-            Answer(participant_id=participant_id, question=question, points=MAX_SCORE).save()
+            try:
+                Answer(participant_id=participant_id, question=question, points=MAX_SCORE).save()
+            except Exception:
+                return redirect(f'/question/{index + 1}')
+            MAX_SCORE = 3
             return redirect(f'/question/{index + 1}')
         context['index_answer'] = index_answer
+        MAX_SCORE = max(0, MAX_SCORE - 1)
 
     return render(request, 'base/game.html', context)
 
